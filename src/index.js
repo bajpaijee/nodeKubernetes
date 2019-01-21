a559410d0fe96efdf1d04a46b3b5677e1f72bd50
shubham-bajpai-xelp <shubham.bajpai@xelpmoc.in>
2019-01-21 20:54:02 +0530

    added a console to test

--- src/index.js
+++ src/index.js
@@ -9,6 +9,6 @@ const myLib = require('./lib')
 
 
 app.get('/', (req, res) => res.send(myLib.helloWorld()))
-
+console.log("HELLO WORLD");
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
 
